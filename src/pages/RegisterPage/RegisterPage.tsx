1522a498485659a7f3ca29bc74d6b520416b7f34
Anton Vynnyk <anton.vynnyk.8118@gmail.com>
2023-08-08 13:10:13 +0300

    update: login and register show error logs

--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -9,64 +9,64 @@ import {useEffect} from "react";
 import {useAppSelector} from "@/hooks/useAppSelector.ts";
 
 const RegisterPage = () => {
-    const dispatch = useAppDispatch();
-    const navigate = useNavigate();
-    const {authorized} = useAppSelector(state => state.userSliceReducer);
+	const dispatch = useAppDispatch();
+	const navigate = useNavigate();
+	const {authorized, error} = useAppSelector(state => state.userSliceReducer);
 
-    const {
-        handleSubmit,
-        register,
-        formState: {errors},
-    } = useForm<AuthData>({
-        mode: "onBlur",
-    });
+	const {
+		handleSubmit,
+		register,
+		formState: {errors},
+	} = useForm<AuthData>({
+		mode: "onBlur",
+	});
 
-    const onSubmit = (data: AuthData) => {
-        dispatch(registerThunk.asyncThunk(data));
-        navigate(home);
-    };
+	const onSubmit = (data: AuthData) => {
+		dispatch(registerThunk.asyncThunk(data));
+		navigate(home);
+	};
 
-    useEffect(() => {
-        if (authorized) {
-            navigate(home);
-        }
-    }, [authorized]);
+	useEffect(() => {
+		if (authorized) {
+			navigate(home);
+		}
+	}, [authorized]);
 
-    return (
-        <div className={styles.formWrapper}>
-
-            <form className={styles.formBlock} onSubmit={handleSubmit(onSubmit)}>
-
-                <p className={styles.formTitle}>Registration</p>
-
-                <div className={styles.fieldsWrapper}>
-                    <label className={styles.formLabel} htmlFor="username">Username
-                        <p className={styles.formError}>{errors?.username &&
-                            <span>{(errors?.username?.message as string) || "Error!"}</span>}
-                        </p>
-                    </label>
-                    <input
-                        {...register("username", {required: "Required field", maxLength: 20, minLength: 4})}
-                        className={styles.formField} id="username" type="text"
-                    />
-
-                    <label className={styles.formLabel} htmlFor="password">Password
-                        <p className={styles.formError}>{errors?.password &&
-                            <span>{(errors?.password?.message as string) || "Error!"}</span>}</p>
-                    </label>
-                    <input
-                        {...register("password", {required: "Required field", minLength: 6})}
-                        className={styles.formField}
-                        id="password" type="password"/>
-                </div>
-
-                <div className={styles.buttonWrapper}>
-                    <a onClick={() => navigate(login)} className={styles.link}>Go to Login</a>
-                    <button type="submit" className={styles.agreeButton}>Registration</button>
-                </div>
-            </form>
-        </div>
-    );
+	return (
+		<div className={styles.formWrapper}>
+			<form className={styles.formBlock} onSubmit={handleSubmit(onSubmit)}>
+				<p className={styles.formTitle}>Registration</p>
+				<div className={styles.fieldsWrapper}>
+					<label className={styles.formLabel} htmlFor="username">Username
+						<p className={styles.formError}>{errors?.username &&
+							<span>{(errors?.username?.message as string) || "Your username length should be from 4 to 20 symbols"}</span>}
+						</p>
+					</label>
+					<input
+						{...register("username", {
+							required: "Required field",
+							maxLength: 20,
+							minLength: 4
+						})}
+						className={styles.formField} id="username" type="text"
+					/>
+					<label className={styles.formLabel} htmlFor="password">Password
+						<p className={styles.formError}>{errors?.password &&
+							<span>{(errors?.password?.message as string) || "Your password min length should be 6 symbols"}</span>}</p>
+					</label>
+					<input
+						{...register("password", {required: "Required field", minLength: 6})}
+						className={styles.formField}
+						id="password" type="password"/>
+				</div>
+				<p className={styles.serverError}>{error}</p>
+				<div className={styles.buttonWrapper}>
+					<a onClick={() => navigate(login)} className={styles.link}>Go to Login</a>
+					<button type="submit" className={styles.agreeButton}>Registration</button>
+				</div>
+			</form>
+		</div>
+	);
 };
 
 export default RegisterPage;
--- src/store/slices/userSlice/slice.ts
+++ src/store/slices/userSlice/slice.ts
@@ -3,30 +3,36 @@ import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 import userSliceAsyncThunks from "./thunks";
 
 export interface UserState {
-    authorized: boolean;
-    user: User | null;
-    accessToken: string | null;
+	authorized: boolean;
+	user: User | null;
+	accessToken: string | null;
+	error: string;
 }
 
 export const initialState: UserState = {
-    authorized: false,
-    user: null,
-    accessToken: null
+	authorized: false,
+	user: null,
+	accessToken: null,
+	error: "",
 };
 
 export const userSlice = createSlice({
-    name: "user_slice",
-    initialState,
-    reducers: {
-        updateUser(state, action: PayloadAction<Partial<UserState>>) {
-            return {...state, ...action.payload};
-        }
-    },
-    extraReducers: (builder) => {
-        for (const thunk of userSliceAsyncThunks) {
-            builder.addCase(thunk.asyncThunk.fulfilled, thunk.storeHandler);
-        }
-    }
+	name: "user_slice",
+	initialState,
+	reducers: {
+		updateUser(state, action: PayloadAction<Partial<UserState>>) {
+			return {...state, ...action.payload};
+		}
+	},
+	extraReducers: (builder) => {
+		for (const thunk of userSliceAsyncThunks) {
+			builder
+				.addCase(thunk.asyncThunk.fulfilled, thunk.storeHandler)
+				.addCase(thunk.asyncThunk.rejected, (state, action) => {
+					state.error = action.payload as string;
+				});
+		}
+	}
 });
 
 export const {updateUser} = userSlice.actions;
