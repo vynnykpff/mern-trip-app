231392207de538ba0802a902e2eb067800929e1a
Anton Vynnyk <anton.vynnyk.8118@gmail.com>
2023-08-09 16:25:48 +0300

    Updates

--- assets/NotFoundPage-1bf1f3b2.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-5d9e78e8.js";const e=()=>o.jsx("div",{children:o.jsx("h2",{children:"NotFoundPage"})});export{e as default};
---
+++ assets/NotFoundPage-70a81577.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-2ebeb38c.js";const e=()=>o.jsx("div",{children:o.jsx("h2",{children:"NotFoundPage"})});export{e as default};
