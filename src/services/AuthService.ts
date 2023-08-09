5cca4876bf73fecde3ff7de8bab9f14562d0dc2c
Anton Vynnyk <anton.vynnyk.8118@gmail.com>
2023-08-09 20:38:20 +0300

    update: logout service

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -4,23 +4,23 @@ import axios, {AxiosResponse} from "axios";
 import {AuthData} from "@/types/AuthData.ts";
 
 export class AuthService {
-    static async login(loginData: AuthData): Promise<AxiosResponse<AuthResponse>> {
-        return $api.post<AuthResponse>("/auth/login", loginData);
-    }
+	static async login(loginData: AuthData): Promise<AxiosResponse<AuthResponse>> {
+		return $api.post<AuthResponse>("/auth/login", loginData);
+	}
 
-    static async register(registerData: AuthData): Promise<AxiosResponse<AuthResponse>> {
-        return $api.post<AuthResponse>("/auth/register", registerData);
-    }
+	static async register(registerData: AuthData): Promise<AxiosResponse<AuthResponse>> {
+		return $api.post<AuthResponse>("/auth/register", registerData);
+	}
 
-    static async refresh(): Promise<AxiosResponse<{ accessToken: string }>> {
-        return $api.post<{ accessToken: string }>("/auth/refresh");
-    }
+	static async refresh(): Promise<AxiosResponse<{ accessToken: string }>> {
+		return $api.post<{ accessToken: string }>("/auth/refresh");
+	}
 
-    static async logout(): Promise<AxiosResponse> {
-        return axios.post(
-            "/auth/logout",
-            {},
-            {withCredentials: true, baseURL: serverURL + "/api"},
-        );
-    }
+	static async logout(): Promise<AxiosResponse> {
+		return axios.post(
+			"/auth/logout",
+			{},
+			{withCredentials: true, baseURL: serverURL + "/api"},
+		);
+	}
 }
